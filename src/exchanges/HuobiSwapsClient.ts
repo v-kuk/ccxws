9d943ca0ce7140cb6f483658c0e1f42a3b41dd81
vladyslav <vladkukler@resonanceholding.com>
2022-10-25 12:27:07 +0300

    run prettier

--- src/exchanges/HuobiSwapsClient.ts
+++ src/exchanges/HuobiSwapsClient.ts
@@ -4,7 +4,7 @@ import { HuobiBase } from "./HuobiBase";
 export class HuobiSwapsClient extends HuobiBase {
     constructor({ wssPath = "wss://api.hbdm.com/swap-ws", watcherMs }: ClientOptions = {}) {
         super({ name: "Huobi Swaps", wssPath, watcherMs });
-        
+
         this.hasLevel2Updates = false;
     }
 }
